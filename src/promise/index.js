e353fa7cb1b5ad9abde083ce429af589e9394b6d
Fernando Poncio <fecrackle@gmail.com>
2020-09-24 23:09:47 -0600

    add promises for the challenge

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!')
+    } else {
+      reject('Whooops!')
+    }
+  })
+}
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error))
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      setTimeout(() => {
+        resolve('True')
+      }, 2000)
+    } else {
+      const error = new Error('Whoops!')
+      reject(error)
+    }
+  })
+}
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err))
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => {
+    console.log('Array of results', response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
